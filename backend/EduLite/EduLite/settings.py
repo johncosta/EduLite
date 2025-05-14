bc148ce2d035a9e314d50eec1ed7ebb2c807a13f
ibrahim abu al roos <ibrahim.sisar@gmail.com>
2025-05-14 23:26:17 +0300

    Enhance security and remove sensitive data

--- backend/EduLite/EduLite/settings.py
+++ backend/EduLite/EduLite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1!xdzfu5tt-q1c^mh5(=6hdw(i%s+jy#!2he0ho6(h)s%#jq_w"
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
