666068226a65efb681d792f3dcfb4dcf5dd31d72
smattymatty <smattymatt@gmail.com>
2025-06-15 23:57:45 -0400

    add create_notification_on_friend_request

--- backend/EduLite/users/signals.py
+++ backend/EduLite/users/signals.py
@@ -3,7 +3,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from .models import UserProfile
+from .models import UserProfile, ProfileFriendRequest
 
 User = get_user_model()
 
@@ -16,3 +16,22 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=ProfileFriendRequest)
+def create_notification_on_friend_request(sender, instance, created, **kwargs):
+    """
+    Signal handler to create a notification when a friend request is sent.
+    - Creates a Notification instance when a new ProfileFriendRequest is created.
+    """
+    if created:
+        # Import here to avoid circular imports
+        from notifications.models import Notification
+
+        Notification.objects.create(
+            recipient=instance.receiver.user,
+            actor=instance.sender.user,
+            verb="sent you a friend request",
+            notification_type="FRIEND_REQUEST",
+            target=instance
+        )
