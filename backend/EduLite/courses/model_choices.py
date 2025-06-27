0f24fa78dc3abdf321c93078b0b56cc6a33ec8d0
pandalow <zxj000hugh@gmail.com>
2025-06-27 19:58:47 +0100

    feat: adding subjects json and load through model choices

--- backend/EduLite/courses/model_choices.py
+++ backend/EduLite/courses/model_choices.py
@@ -45,3 +45,4 @@ def load_choices_from_json(file_name:str) -> List[tuple]:
 OCCUPATION_CHOICES = load_choices_from_json("occupations.json")
 COUNTRY_CHOICES = load_choices_from_json("countries.json")
 LANGUAGE_CHOICES = load_choices_from_json("languages.json")
+SUBJECT_CHOICES = load_choices_from_json("subjects.json")
