5487789dcad2f37975224502b4a4e32ad721d51a
Abdullah Shahid <abdullahshahid@Abdullahs-MacBook-Air.local>
2025-07-15 23:33:29 +0530

    email verification feature added

--- backend/EduLite/users/tokens.py
+++ backend/EduLite/users/tokens.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from six import text_type
 
 class EmailVerificationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
-        return text_type(user.pk) + text_type(timestamp) + text_type(user.is_active)
+        return str(user.pk) + str(timestamp) + str(user.is_active)
 
 account_activation_token = EmailVerificationTokenGenerator()
