c62202d73b7a7b35c10f88f680403a28486a3352
guriWiz <guri.developer97@gmail.com>
2025-07-19 17:41:42 +0530

    Added 'CHANNEL_LAYERS' to use redis with the django channels

--- backend/EduLite/EduLite/settings.py
+++ backend/EduLite/EduLite/settings.py
@@ -253,3 +253,13 @@ CORS_ALLOW_HEADERS = [
     "x-csrftoken",
     "x-requested-with",
 ]
+
+# Django Channels
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
