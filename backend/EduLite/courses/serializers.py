8634c97874243edd77f65b37eeed5752112a67a1
pandalow <zxj000hugh@gmail.com>
2025-08-21 12:17:10 +0100

    feat: adding course serializers

---
+++ backend/EduLite/courses/serializers.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import Group
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from typing import Optional, TYPE_CHECKING
+
